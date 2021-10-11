4a9e8b7074bcc44c9c9cc8b41a4a15bec591fa17
Jonas Bernoulli <jonas@bernoul.li>
2021-10-11 18:06:37 +0200

    Remove redundant find-library-other-{window,frame}
    Also remove `read-library-name'.  Emacs now defines all of these.

--- fwb-cmds.el
+++ fwb-cmds.el
@@ -128,47 +128,6 @@ Only buffers are considered that have a window in the current frame."
           (select-window first-win)
           (if this-win-2nd (other-window 1))))))
 
-(defun read-library-name ()
-  (require 'find-func)
-  (let* ((dirs (or find-function-source-path load-path))
-         (suffixes (find-library-suffixes))
-         (table (apply-partially 'locate-file-completion-table
-                                 dirs suffixes))
-         (def (if (eq (function-called-at-point) 'require)
-                  ;; `function-called-at-point' may return 'require
-                  ;; with `point' anywhere on this line.  So wrap the
-                  ;; `save-excursion' below in a `condition-case' to
-                  ;; avoid reporting a scan-error here.
-                  (condition-case nil
-                      (save-excursion
-                        (backward-up-list)
-                        (forward-char)
-                        (forward-sexp 2)
-                        (thing-at-point 'symbol))
-                    (error nil))
-                (thing-at-point 'symbol))))
-    (when (and def (not (test-completion def table)))
-      (setq def nil))
-    (completing-read (if def (format "Library name (default %s): " def)
-                       "Library name: ")
-                     table nil nil nil nil def)))
-
-;;;###autoload
-(defun find-library-other-window (library)
-  "Find the Emacs-Lisp source of LIBRARY in another window."
-  (interactive (list (read-library-name)))
-  (let ((buf (find-file-noselect (find-library-name library))))
-    (pop-to-buffer buf 'other-window)))
-
-;;;###autoload
-(defun find-library-other-frame (library)
-  "Find the Emacs-Lisp source of LIBRARY in another frame."
-  (interactive (list (read-library-name)))
-  (let ((buf (find-file-noselect (find-library-name library))))
-    (condition-case nil
-        (switch-to-buffer-other-frame buf)
-      (error (pop-to-buffer buf)))))
-
 ;;;###autoload
 (defun sudo-find-file (&optional arg)
   (interactive "P")
