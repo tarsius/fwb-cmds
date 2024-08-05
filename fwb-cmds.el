973a0033c406ce644ce0fb87de70c2f4046ab1b7
Jonas Bernoulli <jonas@bernoul.li>
2024-08-05 15:18:14 +0200

    Release version 2.0.2

--- fwb-cmds.el
+++ fwb-cmds.el
@@ -6,6 +6,7 @@
 ;; Homepage: https://github.com/tarsius/fwb-cmds
 ;; Keywords: convenience
 
+;; Package-Version: 2.0.2
 ;; Package-Requires: ((emacs "26.1") (compat "30.0.0.0"))
 
 ;; SPDX-License-Identifier: GPL-3.0-or-later
